92108e437dd5edf64bdaea83c130ea846ddcaa9d
Fahad <fahad.waseem102@gmail.com>
2018-11-01 13:05:21 -0700

    Some Changes Made

--- app/src/main/java/com/example/fahad/objectpassingapplication/Main2Activity.java
+++ app/src/main/java/com/example/fahad/objectpassingapplication/Main2Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -16,5 +17,6 @@ public class Main2Activity extends AppCompatActivity {
         Student student = (Student) getIntent().getSerializableExtra("Data");
         textView_result = findViewById(R.id.textView_result) ;
         textView_result.setText(student.toString());
+        Toast.makeText(this, "Registered Successfully !", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/fahad/objectpassingapplication/MainActivity.java
+++ app/src/main/java/com/example/fahad/objectpassingapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fahad.objectpassingapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText et_name, et_father_name, et_city, et_age, et_rollno ;
-    Button btn ;
+    EditText et_name, et_father_name, et_city, et_age, et_rollno;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,27 +21,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         setBounds();
     }
-    public void setBounds(){
-        et_name = findViewById(R.id.et_name) ;
-        et_father_name = findViewById(R.id.et_father) ;
-        et_city = findViewById(R.id.et_city) ;
-        et_age = findViewById(R.id.et_age) ;
-        et_rollno = findViewById(R.id.et_roll) ;
-        btn = findViewById(R.id.btn) ;
+
+    public void setBounds() {
+        et_name = findViewById(R.id.et_name);
+        et_father_name = findViewById(R.id.et_father);
+        et_city = findViewById(R.id.et_city);
+        et_age = findViewById(R.id.et_age);
+        et_rollno = findViewById(R.id.et_roll);
+        btn = findViewById(R.id.btn);
 
         btn.setOnClickListener(this);
     }
 
+
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class) ;
-        Student student = new Student() ;
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        Student student = new Student();
         student.setName(et_name.getText().toString());
         student.setFathers_name(et_father_name.getText().toString());
         student.setCity(et_city.getText().toString());
         student.setAge(Integer.parseInt(et_age.getText().toString()));
         student.setRoll_no(Integer.parseInt(et_rollno.getText().toString()));
-        intent.putExtra("Data", student) ;
+        intent.putExtra("Data", student);
         startActivity(intent);
+
+        et_name.setText("");
+        et_father_name.setText("");
+        et_rollno.setText("");
+        et_age.setText("");
+        et_city.setText("");
     }
 }
